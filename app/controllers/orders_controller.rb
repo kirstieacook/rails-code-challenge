a139ea07bf5b237c1a09bff5793a4a867de29d2d
Kirstie Cook <kirstie@travelershaven.com>
2018-10-27 17:20:54 -0600

    add new/create actions and views for orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,4 +7,18 @@ class OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
   end
+
+  def new
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(params[:order])
+
+    if @order.save
+      redirect_to @order
+    else
+      render action: 'new'
+    end
+  end
 end
