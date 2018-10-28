f02574b5fd93ad30970793e8630fa1f895f79723
Kirstie Cook <kirstie@travelershaven.com>
2018-10-27 19:08:25 -0600

    add widget seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,6 @@
 
 # Shipped at times are randomly assigned.  What great randomization I've done!
 5.times { Order.create!(shipped_at: Time.now + rand(40).minute) }
+
+# Generate some widgets!
+5.times { Widget.create!(name: Faker::Appliance.equipment) }
